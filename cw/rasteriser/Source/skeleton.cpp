f2fa8362673efec0e86a7db32a7ef962e10ad5fc
TristanSaunders <tristansaundersfr@gmail.com>
2018-02-19 17:42:27 +0000

    rotation almost done, has bug

--- cw/rasteriser/Source/skeleton.cpp
+++ cw/rasteriser/Source/skeleton.cpp
@@ -5,12 +5,13 @@
 #include "TestModelH.h"
 #include <stdint.h>
 
-using std::vector;
+using namespace std;
 using glm::vec3;
 using glm::mat3;
 using glm::vec4;
 using glm::mat4;
 using glm::ivec2;
+using glm::vec2;
 
 namespace std
 {
@@ -28,6 +29,7 @@ namespace std
 #define SCREEN_WIDTH 320
 #define SCREEN_HEIGHT 320
 #define FULLSCREEN_MODE false
+#define CHECKING_KEY_STATE true
 #define FOCAL_LENGTH (SCREEN_HEIGHT*0.98)
 
 struct Intersection
@@ -41,12 +43,13 @@ struct Intersection
 /* ----------------------------------------------------------------------------*/
 /* FUNCTIONS                                                                   */
 
-void Update();
+void Update(vec4& cameraPos, mat4& cameraDirection);
 void Draw(screen* screen);
 void VertexShader( vec4 vertices, ivec2& projPos );
 void DrawLineSDL( SDL_Surface* surface, ivec2 a, ivec2 b, vec3 color );
 void Interpolate( ivec2 a, ivec2 b, vector<ivec2>& result );
 void DrawPolygonEdges( const vector<vec4>& vertices , screen* screen);
+mat4 rotation(float yaw);
 
 /*
   ---------------------------------------------------
@@ -55,30 +58,33 @@ void DrawPolygonEdges( const vector<vec4>& vertices , screen* screen);
 
 vector<Triangle> triangles;
 vec4 cameraPos( 0, 0, -3.001,1);
+glm::mat4 R;
+float yaw = 0; // Yaw angle controlling camera rotation around y-axis
+mat4 cameraDirection =  rotation(0);
 
-void VertexShader( vec4 vertices, ivec2& projPos ) {
+void VertexShader(vec4 vertices, ivec2& projPos) {
   // std::cout << vertices << std::endl;
-  vertices = vec4(vertices - cameraPos);
+  vertices = vec4(cameraDirection*vec4(vertices - cameraPos));
   projPos.x = (FOCAL_LENGTH * (vertices.x)/(vertices.z)) + (SCREEN_WIDTH/2);
   projPos.y = (FOCAL_LENGTH * (vertices.y)/(vertices.z)) + (SCREEN_HEIGHT/2);
   // std::cout << projPos << std::endl;
 }
 
-int main( int argc, char* argv[] )
+int main(int argc, char* argv[])
 {
 
-  screen *screen = InitializeSDL( SCREEN_WIDTH, SCREEN_HEIGHT, FULLSCREEN_MODE );
+  screen *screen = InitializeSDL(SCREEN_WIDTH, SCREEN_HEIGHT, FULLSCREEN_MODE);
   LoadTestModel(triangles);
 
 
-  while( NoQuitMessageSDL() )
+  while(NoQuitMessageSDL())
     {
-      Update();
+      Update(cameraPos, cameraDirection);
       Draw(screen);
       SDL_Renderframe(screen);
     }
 
-  SDL_SaveImage( screen, "screenshot.bmp" );
+  SDL_SaveImage(screen, "screenshot.bmp");
 
   KillSDL(screen);
   return 0;
@@ -101,9 +107,14 @@ void Draw(screen *screen)
   }
 }
 /*Place updates of parameters here*/
-void Update()
+void Update(vec4& cameraPos, mat4& cameraDirection)
 {
   static int t = SDL_GetTicks();
+
+  vec4 right(cameraDirection[0][0], cameraDirection[0][1], cameraDirection[0][2], 1);
+  vec4 down(cameraDirection[1][0], cameraDirection[1][1], cameraDirection[1][2], 1);
+  vec4 forward( cameraDirection[2][0], cameraDirection[2][1], cameraDirection[2][2], 1);
+
   /* Compute frame time */
   int t2 = SDL_GetTicks();
   float dt = float(t2-t);
@@ -111,6 +122,44 @@ void Update()
   /*Good idea to remove this*/
   std::cout << "Render time: " << dt << " ms." << std::endl;
   /* Update variables*/
+
+  if(CHECKING_KEY_STATE){
+     const uint8_t* keystate = SDL_GetKeyboardState(0);
+
+     if(keystate == NULL){
+       printf("Keys are NULL \n");
+     }
+     else {
+ //Move Camera
+       if(keystate[SDL_SCANCODE_UP]){
+         cameraPos += (forward * 0.05f);
+       }
+       if(keystate[SDL_SCANCODE_DOWN]){
+         cameraPos -= (forward * 0.05f);
+       }
+       if(keystate[SDL_SCANCODE_LEFT]){
+         yaw -= 0.04;
+         cameraDirection = rotation(yaw);
+       }
+       if(keystate[SDL_SCANCODE_RIGHT]){
+         yaw += 0.04;
+         cameraDirection = rotation(yaw);
+       }
+ // //Move Light Source
+ //       if(keystate[SDL_SCANCODE_W]){
+ //         lightPositon += (forward*0.05f);
+ //       }
+ //       if(keystate[SDL_SCANCODE_S]){
+ //         lightPositon -= (forward*0.05f);
+ //       }
+ //       if(keystate[SDL_SCANCODE_A]){
+ //         lightPositon -= (right*0.05f);
+ //       }
+ //       if(keystate[SDL_SCANCODE_D]){
+ //         lightPositon += (right*0.05f);
+ //       }
+     }//end of large else
+   }
 }
 
 void Interpolate( ivec2 a, ivec2 b, vector<ivec2>& result )
@@ -148,4 +197,12 @@ void DrawPolygonEdges(const vector<vec4>& vertices , screen* screen)
     vec3 color(1, 1, 1);
     DrawLineSDL(screen, projectedVertices[i], projectedVertices[j], color);
   }
+}
+
+mat4 rotation(float yaw){
+  mat4 R_y(cos(yaw), 0, sin(yaw), 0,
+              0    , 1,    0    , 0,
+          -sin(yaw), 0, cos(yaw), 0,
+              0    , 0,    0    , 1);
+  return R_y;
 }
