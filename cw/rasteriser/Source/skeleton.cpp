ae37f2c096f4ef54522836e6d8c32fd1b85da8c3
TristanSaunders <tristansaundersfr@gmail.com>
2018-02-19 16:39:22 +0000

    done interpolation

--- cw/rasteriser/Source/skeleton.cpp
+++ cw/rasteriser/Source/skeleton.cpp
@@ -26,7 +26,7 @@ namespace std
 };
 
 #define SCREEN_WIDTH 320
-#define SCREEN_HEIGHT 256
+#define SCREEN_HEIGHT 320
 #define FULLSCREEN_MODE false
 #define FOCAL_LENGTH (SCREEN_HEIGHT*0.98)
 
@@ -44,6 +44,9 @@ struct Intersection
 void Update();
 void Draw(screen* screen);
 void VertexShader( vec4 vertices, ivec2& projPos );
+void DrawLineSDL( SDL_Surface* surface, ivec2 a, ivec2 b, vec3 color );
+void Interpolate( ivec2 a, ivec2 b, vector<ivec2>& result );
+void DrawPolygonEdges( const vector<vec4>& vertices , screen* screen);
 
 /*
   ---------------------------------------------------
@@ -85,20 +88,17 @@ void Draw(screen *screen)
 {
   /* Clear buffer */
   memset(screen->buffer, 0, screen->height*screen->width*sizeof(uint32_t));
-  for( uint32_t i=0; i<triangles.size(); ++i )
+  for(uint32_t i=0; i<triangles.size(); ++i)
   {
     vector<vec4> vertices(3);
+    vector<ivec2> vecProjPos(3);
     vertices[0] = triangles[i].v0;
     vertices[1] = triangles[i].v1;
     vertices[2] = triangles[i].v2;
-    for(int v=0; v<3; ++v)
-    {
-      ivec2 projPos; // x and y
-      VertexShader( vertices[v], projPos );
-      vec3 color(1,1,1);
-      PutPixelSDL( screen, projPos.x, projPos.y, color);
-      }
-    }
+
+    DrawPolygonEdges(vertices, screen);
+
+  }
 }
 /*Place updates of parameters here*/
 void Update()
@@ -111,4 +111,41 @@ void Update()
   /*Good idea to remove this*/
   std::cout << "Render time: " << dt << " ms." << std::endl;
   /* Update variables*/
+}
+
+void Interpolate( ivec2 a, ivec2 b, vector<ivec2>& result )
+{
+  int N = result.size();
+  glm::vec2 step = glm::vec2(b-a) / float(glm::max(N-1,1));
+  glm::vec2 current( a );
+  for( int i=0; i<N; ++i ){
+    result[i] = current;
+    current += step;
+  }
+}
+
+void DrawLineSDL( screen* screen, ivec2 a, ivec2 b, vec3 color ){
+  ivec2 delta = glm::abs(a - b);
+  int pixels = glm::max(delta.x, delta.y) + 1;
+  vector<ivec2> line(pixels);
+  Interpolate(a, b, line);
+  for(int i = 0; i < line.size(); i++){
+    PutPixelSDL(screen, line[i].x, line[i].y, color);
+  }
+}
+
+void DrawPolygonEdges(const vector<vec4>& vertices , screen* screen)
+{
+  int V = vertices.size();
+  // Transform each vertex from 3D world position to 2D image position:
+  vector<ivec2> projectedVertices(V);
+  for(int i=0; i<V; ++i){
+    VertexShader(vertices[i], projectedVertices[i]);
+  }
+  // Loop over all vertices and draw the edge from it to the next vertex:
+  for(int i=0; i<V; ++i){
+    int j = (i+1)%V; // The next vertex
+    vec3 color(1, 1, 1);
+    DrawLineSDL(screen, projectedVertices[i], projectedVertices[j], color);
+  }
 }
